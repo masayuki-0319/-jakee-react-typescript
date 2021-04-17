1f02e9091efc973391e69dd119b38322d6271293
Masayuki Onishi <omccello@gmail.com>
2021-04-17 14:32:46 +0900

    ログイン画面のボタンを切り出し

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
 import React, { memo } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+
+import PrimaryButton from "../atoms/button/PrimaryButton";
 
 export const Login: React.FC = memo(() => {
   return (
@@ -19,9 +13,7 @@ export const Login: React.FC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placehodler="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
